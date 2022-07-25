a1c0124c5b82cb86de0583920c4f6b559f0fdc98
Fix3dP0int <105424474+Fix3dP0int@users.noreply.github.com>
2022-07-25 09:59:42 +0800

    implement the naïve birthday attack of reduced SM3
    调用gmssl库中的sm3算法进行生日攻击

--- SM3bygcl/SM3biratt.py
+++ SM3bygcl/SM3biratt.py
@@ -1,28 +1,19 @@
-from SM3FromInter import *
 from random import *
-
+from gmssl import sm3
 DLEN = 4
 def birthday():
     count = 0
     while(1):
-        m0 = Fill(str(randint(0, 2 ** 32)))
-        m1 = Fill(str(randint(0, 2 ** 32)))
-        M0 = Group(m0)
-        M1 = Group(m1)
-        Vn0 = Iterate(M0)
-        Vn1 = Iterate(M1)
-        result0 = ''
-        result1 = ''
-        for x in Vn0:
-            result0 += hex(x)[2:]
-        for x in Vn1:
-            result1 += hex(x)[2:]
+        temp0 = randint(0, 2 ** 32)
+        temp1 = randint(0, 2 ** 32)
+        digest0 = sm3.sm3_hash([temp0])
+        digest1 = sm3.sm3_hash([temp1])
         count = count + 1
         print("count:",count)
-        if result0[0:DLEN] == result1[0:DLEN]:
+        if digest0[0:DLEN] == digest1[0:DLEN]:
             print("find the collision!")
-            print("m0:",m0)
-            print("m1:",m1)
+            print("m0:",temp0)
+            print("m1:",temp1)
             break
 
 if __name__ == "__main__":
