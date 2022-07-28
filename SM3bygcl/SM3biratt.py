3b4a3186e590baaa3bdeb9d9e8db06323b8490d3
Fix3dP0int <105424474+Fix3dP0int@users.noreply.github.com>
2022-07-28 14:12:01 +0800

    Update SM3biratt.py

--- SM3bygcl/SM3biratt.py
+++ SM3bygcl/SM3biratt.py
@@ -1,20 +1,32 @@
 from random import *
 from gmssl import sm3
-DLEN = 4
+DLEN = 8
+def s2l(s):
+    return [int(s[i], base = 16) for i in range(0,64)]
+
 def birthday():
-    count = 0
+    x = [randint(0, 2 ** 32)]
+    h = dict()
+    temp = x[:]
     while(1):
-        temp0 = randint(0, 2 ** 32)
-        temp1 = randint(0, 2 ** 32)
-        digest0 = sm3.sm3_hash([temp0])
-        digest1 = sm3.sm3_hash([temp1])
-        count = count + 1
-        print("count:",count)
-        if digest0[0:DLEN] == digest1[0:DLEN]:
-            print("find the collision!")
-            print("m0:",temp0)
-            print("m1:",temp1)
+        m = temp[:]
+        temp = sm3.sm3_hash(temp)
+        if (temp[0:DLEN] in h):
             break
+        h[temp[0:DLEN]] = m
+        temp = s2l(temp)
+    print("when finding the collision, the amount of hashvalue set : ", len(h))
+    print("the collision hash value is : ", temp)
+    print("Ma : ", m)
+    mb = h[temp[0:DLEN]]
+    print("Mb : ", mb)
+    realhasha = sm3.sm3_hash(m)
+    realhashb = sm3.sm3_hash(mb)
+    print("the real hash value of Ma : ", realhasha)
+    print("the real hash value of Mb : ", realhashb)
+    if realhasha[0:DLEN] == realhashb[0:DLEN]:
+        print("True")
+
 
 if __name__ == "__main__":
     birthday()
