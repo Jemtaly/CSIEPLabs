291426bc65d4a78ded9e23f54f1d0a3c8050b0ee
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-07-28 09:35:21 +0800

    Add files via upload

--- SM2/sm2.py
+++ SM2/sm2.py
@@ -1,23 +1,49 @@
 import random
-import pysmx.SM3
+import pysmx
 
 
 def kdf(zin, klen):
     rcnt = (klen - 1) // 32 + 1
-    ha = b''.join(pysmx.SM3.digest(zin + ct.to_bytes(4, 'big')) for ct in range(1, rcnt + 1))
+    ha = b''.join(pysmx.SM3.digest(zin + ct.to_bytes(4, 'big'))
+                  for ct in range(1, rcnt + 1))
     return ha[:klen]
 
 
+def isprime(n):
+    if n == 2:
+        return True
+    if n < 2 or n & 1 == 0:
+        return False
+    for _ in range(16):
+        a = random.randrange(1, n)
+        d = n - 1
+        while d & 1 == 0:
+            t = pow(a, d, n)
+            if t == n - 1:
+                break
+            if t != 1:
+                return False
+            d >>= 1
+    return True
+
+
 class SM2:
-    def __init__(self, a, b, n, p, G, paralen):
-        self.n = n
-        self.a = a
-        self.b = b
+    def __init__(self, m, a, b, p, n, G):
+        self.m = m
+        assert isprime(p) and 2 < p < 256 ** m and \
+            (4 * a * a * a + 27 * b * b) % p != 0
+        self.a = a % p
+        self.b = b % p
         self.p = p
-        self.G = G
-        self.paralen = paralen
+        assert isprime(n) and n > 2 ** 191 and n * n > 16 * p and \
+            self.check(G) and self.mult(n, G) == None
+        self.n = n % p
+        self.G = G[0] % p, G[1] % p
         self.d = random.randrange(1, n - 1)
         self.P = self.mult(self.d, G)
+
+    def check(self, P):
+        return not P or (P[0] * P[0] * P[0] - P[1] * P[1] + self.a * P[0] + self.b) % self.p == 0
 
     def add(self, P, Q):
         if not P:
@@ -49,11 +75,11 @@ class SM2:
             x = self.mult(k, self.G)[0]
             r = (e + x) % self.n
             s = pow(self.d + 1, self.n - 2, self.n) * (k - r * self.d) % self.n
-        return r.to_bytes(self.paralen, 'big') + s.to_bytes(self.paralen, 'big')
+        return r.to_bytes(self.m, 'big') + s.to_bytes(self.m, 'big')
 
     def verify(self, S, M):
-        r, s = int.from_bytes(S[:self.paralen], 'big'), int.from_bytes(
-            S[self.paralen:], 'big')
+        r = int.from_bytes(S[:self.m], 'big')
+        s = int.from_bytes(S[self.m:], 'big')
         if not (0 < r < self.n and 0 < s < self.n):
             return False
         e = int.from_bytes(M, 'big')
@@ -70,52 +96,27 @@ class SM2:
         while not any(t):
             k = random.randrange(1, self.n)
             kP = self.mult(k, self.P)
-            x2, y2 = kP[0].to_bytes(self.paralen, 'big'), kP[1].to_bytes(self.paralen, 'big')
+            x2, y2 = kP[0].to_bytes(self.m, 'big'), kP[1].to_bytes(self.m, 'big')
             t = kdf(x2 + y2, klen)
         kG = self.mult(k, self.G)
-        x1, y1 = kG[0].to_bytes(self.paralen, 'big'), kG[1].to_bytes(self.paralen, 'big')
+        x1, y1 = kG[0].to_bytes(self.m, 'big'), kG[1].to_bytes(self.m, 'big')
         C1 = x1 + y1
         C2 = bytes(a ^ b for a, b in zip(M, t))
         C3 = pysmx.SM3.digest(x2 + M + y2)
         return C1 + C2 + C3
 
     def decrypt(self, C):
-        l2 = 2 * self.paralen
+        l2 = 2 * self.m
         C1 = C[:l2]
         C2 = C[l2:-32]
         C3 = C[-32:]
         klen = len(C2)
-        x1, x2 = C1[:self.paralen], C1[self.paralen:]
+        x1, x2 = C1[:self.m], C1[self.m:]
         kG = int.from_bytes(x1, 'big'), int.from_bytes(x2, 'big')
         kP = self.mult(self.d, kG)
-        x2, y2 = kP[0].to_bytes(self.paralen, 'big'), kP[1].to_bytes(self.paralen, 'big')
+        x2, y2 = kP[0].to_bytes(self.m, 'big'), kP[1].to_bytes(self.m, 'big')
         t = kdf(x2 + y2, klen)
         assert any(t)
         M = bytes(a ^ b for a, b in zip(C2, t))
         assert pysmx.SM3.digest(x2 + M + y2) == C3
         return M
-
-
-def test():
-    a = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
-    b = 0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
-    p = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
-    n = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
-    G = 0x32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7, 0xbc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0
-    sm2 = SM2(a, b, n, p, G, 32)
-    m = b'abc'
-    c = sm2.encrypt(m)
-    p = sm2.decrypt(c)
-    print('------------------------ Encrypt and Decrypt ------------------------')
-    print('m = 0x' + m.hex())
-    print('c = 0x' + c.hex())
-    print('p = 0x' + p.hex())
-    print('Decryption succeeded.' if p == m else 'Decryption failed.')
-    print('-------------------------- Sign and Verify --------------------------')
-    s = sm2.sign(m)
-    print('s = 0x' + s.hex())
-    print('Verification succeeded.' if sm2.verify(s, m) else 'Verification failed.')
-
-
-if __name__ == '__main__':
-    test()
---
+++ SM2/sm2_test.py
@@ -0,0 +1,35 @@
+import sm2
+
+
+def sm2_test():
+    a = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
+    b = 0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
+    P = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
+    n = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
+    G = 0x32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7, 0xbc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0
+    print('a = 0x' + a.to_bytes(32, 'big').hex())
+    print('b = 0x' + b.to_bytes(32, 'big').hex())
+    print('p = 0x' + P.to_bytes(32, 'big').hex())
+    print('n = 0x' + n.to_bytes(32, 'big').hex())
+    print('G_x = 0x' + G[0].to_bytes(32, 'big').hex())
+    print('G_y = 0x' + G[1].to_bytes(32, 'big').hex())
+    sm2obj = sm2.SM2(32, a, b, P, n, G)
+    print('d = 0x' + sm2obj.d.to_bytes(32, 'big').hex())
+    print('P_x = 0x' + sm2obj.P[0].to_bytes(32, 'big').hex())
+    print('P_y = 0x' + sm2obj.P[1].to_bytes(32, 'big').hex())
+    M = b'abc'
+    print('M = 0x' + M.hex())
+    print('-------------------------- Encrypt and Decrypt --------------------------')
+    C = sm2obj.encrypt(M)
+    print('C = 0x' + C.hex())
+    P = sm2obj.decrypt(C)
+    print('P = 0x' + P.hex())
+    print('Decryption succeeded.' if P == M else 'Decryption failed.')
+    print('---------------------------- Sign and Verify ----------------------------')
+    S = sm2obj.sign(M)
+    print('S = 0x' + S.hex())
+    print('Verification succeeded.' if sm2obj.verify(S, M) else 'Verification failed.')
+
+
+if __name__ == '__main__':
+    sm2_test()
